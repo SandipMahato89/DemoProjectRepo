363bba3b3da9e5be9dbf67834c8a62cbd17c82c9
Sandip Mahato <sing91816@gmail.com>
2024-08-10 18:33:38 +0530

    intial message

---
+++ src/main/java/com/cg/DemoProjectRepo1Application.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectRepo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectRepo1Application.class, args);
+	}
+
+}
